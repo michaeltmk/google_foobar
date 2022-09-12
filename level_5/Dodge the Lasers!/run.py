c2f4eb8634c6b3c9456cd49d7eabf557de3e1cbd
michael tse <tse.man.kit.michael@gmail.com>
2022-09-12 23:03:50 +0800

    feat:complete

--- level_5/Dodge the Lasers!/run.py
+++ level_5/Dodge the Lasers!/run.py
@@ -1,7 +1,7 @@
 import solution
 import random
-from math import sqrt, floor
 from decimal import Decimal
+
 
 def test_solution():
     assert solution.solution('77') == "4208"
@@ -11,18 +11,22 @@ def test_sum_of_beatty():
     assert solution.sum_of_beatty(Decimal(2).sqrt() , Decimal(77)) == 4208
     assert solution.sum_of_beatty(Decimal(2).sqrt(), Decimal(5)) == 19
     assert solution.sum_of_beatty(Decimal(2).sqrt(), Decimal(0)) == 0
-    for Q in range(2,5):
-        print(Q)
-        assert solution.sum_of_beatty(sqrt(Q), 1) == floor(sqrt(Q)) , f"{Q=}"
 
-def _test_speed():
+def test_speed():
     for i in range(100):
         n = Decimal(random.randint(10*90,10**100))
         assert solution.solution(str(n)) == str(int(solution.sum_of_beatty(Decimal(2).sqrt(),n)))
 
+def _test_compare():
+    for _ in range(100):
+        n = Decimal(random.randint(10*90,10**100))
+        assert solution.solution(str(n)) == solution2.solution(str(n))
+
 if __name__ == '__main__':
     test_solution()
+    test_sum_of_beatty()
+    test_speed()
     # for _ in range(100):
     #     print(solution.solution(str(random.randint(10**99,10**100))))
-    for i in range(100):
-        print(solution.solution(str(i)))
+    # for i in range(100):
+    #     print(solution.solution(str(i)))
--- level_5/Dodge the Lasers!/solution.py
+++ level_5/Dodge the Lasers!/solution.py
@@ -1,25 +1,15 @@
-from decimal import Decimal, getcontext
-from math import floor
-
-getcontext().prec = 101
-root2 = Decimal(2).sqrt()
-states = (root2,root2+1,root2+2)
-root2_minus1 = states[0]-1
+from decimal import Decimal, localcontext
 
 def solution(str_n):
-    n = Decimal(str_n)
-    a = states[0]
-    return str(int(sum_of_beatty(a,n)))
+    n = int(str_n)
+    with localcontext() as ctx:
+        ctx.prec = 102
+        a = Decimal(2).sqrt()
+        return str(int(sum_of_beatty(a,n)))
 
 def sum_of_beatty(a,n):
     if n == 0:
         return 0
-    if n == 1:
-        return floor(a)
-    nd = floor(root2_minus1*n)
-    nd = Decimal(nd)
-    b = states[-1]
-    if a == states[-1]:
-        return (n+nd)*(n+nd+1)/2 + n*(n+1) - sum_of_beatty(b,nd)
-    elif a == states[0]:
-        return (n+nd)*(n+nd+1)/2 - sum_of_beatty(b,nd)
+    m = int(a*n)
+    nd = int((a-1)*n)
+    return (m*(m+1))/2 - sum_of_beatty(a,nd)  - nd*(nd+1)
